544b26ea7c018914d4dc2a0e1bf9438cc0365f56
Anemosfy <164875986+Anemosfy@users.noreply.github.com>
2024-11-29 16:04:58 +0600

    Create manjaro-xfce-vnc.sh

---
+++ manjaro/manjaro-xfce-vnc.sh
@@ -0,0 +1,7 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+export XDG_RUNTIME_DIR=${TMPDIR}
+sleep 2
+proot-distro login manjaro --shared-tmp -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - <username> -c "env DISPLAY=:0 startxfce4 vncserver :1"'
+exit 0
